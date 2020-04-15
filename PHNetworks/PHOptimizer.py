e2760cd3d731ef2b7508c14fb14fb86b8a3fbf40
Daniel Albert <esclear@users.noreply.github.com>
2020-04-16 00:47:32 +0200

    Initial commit

---
+++ PHNetworks/PHOptimizer.py
@@ -0,0 +1,329 @@
+# Optimizer for TensorFlow models utilizing Port-Hamiltonian systems
+# Copyright (C) 2020  Daniel Albert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""A TensorFlow Optimizer for neural networks based on the paper
+    'Port-Hamiltonian Approach to Neural Network Training'
+    by Massaroli, Poli, et. al.
+    arXiv:1909.02702v1 [cs.NE] – https://arxiv.org/abs/1909.02702v1
+
+    This can't be used in-place instead of the optimizers provided in
+    TensorFlow, since the interfaces are not compatible.
+"""
+# For type anotations
+from typing import List
+
+# For functionality
+import tensorflow as tf
+import tensorflow.keras as keras
+from scipy import sparse
+from scipy.integrate import solve_ivp
+import numpy as np
+
+
+class PortHamiltonianOptimizer:
+    def __init__(
+            self,
+            alpha: float = 1.0,
+            beta: float = 0.0,
+            gamma: float = 1.0,
+            resistive: float = 0.5,
+            ivp_period: float = 1.0,
+            ivp_steps: int = None,
+            ivp_solver: str = 'RK45',
+        ):
+        """TODO: DOCUMENTATION"""
+
+        # Optimizer hyperpameters
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.resistive = resistive
+
+        # Configuration of the IVP solver
+        self.ivp_period = ivp_period
+        self.ivp_solver = ivp_solver
+        self.ivp_t_span = np.linspace(0.0, self.ivp_period, ivp_steps) if ivp_steps else (0.0, self.ivp_period)
+
+        # Initialize internal parameters
+        self._last_model = None
+        self._momenta = None
+        self._param_count = None
+        self._dynamics_matrix = None
+
+    def _check_model_and_state(self, model: keras.models.Model):
+        """Hook to be called on training, this checks whether the model being trained was changed"""
+
+        if model != self._last_model:
+            # Set new model
+            self._last_model = model
+
+            param_count = _model_param_count(model)
+
+            self._param_count = param_count
+            self._momenta = tf.zeros([param_count], dtype="float64")
+
+            B = self.resistive * sparse.eye(param_count)
+            self._dynamics_matrix = PortHamiltonianOptimizer._calculate_dynamics_matrix(B)
+            del B
+
+    # Training related functionality
+    def train(
+            self,
+            model: keras.models.Model,
+            input_data,
+            target_data,
+            epochs: int = 1,
+            batch_size: int = 64,
+            randomize_batches: bool = True,
+            metrics=[],
+            hamiltonian_generator=None
+        ) -> None:
+        """TODO: DOCUMENTATION"""
+
+        # Determine the number of samples within the training dataset
+        sample_cnt = input_data.shape[0]
+        assert target_data.shape[0] == sample_cnt
+
+        train_dataset = tf.data.Dataset.from_tensor_slices((input_data, target_data))
+        train_dataset = train_dataset.shuffle(
+            buffer_size=1024, reshuffle_each_iteration=randomize_batches
+        ).batch(batch_size)
+
+        for epoch in range(1, epochs + 1):
+            print(f'Epoch {epoch}/{epochs}')
+            pbar = keras.utils.Progbar(sample_cnt, stateful_metrics=['loss'])
+
+            for m in metrics:
+                m.reset_states()
+
+            in_epoch = 0
+            for batch in train_dataset:
+                loss, energy = self.train_batch(model, *batch, metrics, hamiltonian_generator)
+
+                # Update the progress bar wit the current number of processed samples and the latest metrics
+                in_epoch += batch[0].shape[0]
+                pbar.update(
+                    in_epoch, [('loss', loss), ('energy', energy)] + [(m.name, m.result()) for m in metrics]
+                )
+
+    def train_batch(
+            self, model: keras.models.Model, input_batch, target_batch,
+            metrics=[], hamiltonian_generator=None
+        ) -> float:
+        """TODO: DOCUMENTATION"""
+
+        # Basic sanity check that input sample count matches target sample
+        # count
+        assert input_batch.shape[0] == target_batch.shape[0]
+
+        self._check_model_and_state(model)
+        p = self._param_count
+
+        params = _flatten_variables(model.trainable_variables)
+        state = tf.concat([params, self._momenta], 0)
+
+        dynamics, hamiltonian = self.get_dynamics(model, input_batch, target_batch, hamiltonian_generator)
+
+        # Solve the IVP problem using SciPy's IVP solver.
+        solution = solve_ivp(dynamics, self.ivp_t_span, state, method=self.ivp_solver)
+        # TODO: Check solution
+
+        # The new state resulting from the evolution of the IVP
+        new_state = solution.y[:, -1]
+
+        # Split the network parameters and evolution momenta from the new state
+        params = new_state[:p]
+        momenta = new_state[p:]
+
+        _model_set_flat_variables(model, params)
+        self._momenta = momenta
+
+        prediction = model(input_batch)
+        for metric in metrics:
+            metric.update_state(target_batch, prediction)
+
+        return model.loss(target_batch, prediction), hamiltonian(params, momenta)
+
+    def get_dynamics(
+            self, model: keras.models.Model, inputs, targets, hamiltonian_generator=None
+        ):
+        """This function returns a tf.function that defines the dynamics of a PH system.
+        These are defined as ẋ=(J-R)·∇H(x)=F·∇H(x), where F the dynamics matrix calculated when a new model is loaded."""
+
+        self._check_model_and_state(model)
+
+        p = self._param_count
+        F = self._dynamics_matrix
+
+        hamiltonian = (hamiltonian_generator or self.get_hamiltonian)(model, inputs, targets)
+
+        # As long as no custom hamiltonian function is given, the first
+        # dynamics function is used, which has some features of the
+        # gradient calculated by hand, which results in a speed
+        # improvement of about 390 % in tests.
+        # In case a custom hamiltonan function is used, the gradient
+        # must be determined by the gradient tape as well, which is
+        # slower.
+        if hamiltonian_generator is None:
+            # This is the function which describes the parameter dynamics and
+            # will be used for integration
+            def dynamics(t, state):
+                state = tf.convert_to_tensor(state)
+
+                params = state[:p]
+                momenta = state[p:]
+
+                _model_set_flat_variables(model, params)
+
+                with tf.GradientTape() as tape:
+                    tape.watch(state)
+
+                    loss = model.loss(targets, model(inputs))
+
+                loss_gradient = _flatten_variables(
+                    tape.gradient(loss, model.trainable_variables)
+                )
+
+                # The hamiltonian is H(x) = H(ϑ,ω) = ½·( α·loss(ϑ) + β·∥ϑ∥² + γ·∥ω∥² ) = ½·( α·loss(ϑ) + β·ϑᵀ·ϑ + γ·ωᵀ·ω )
+                #   Thus, ∇_ϑ H = ½·α·∇_ϑ loss(ϑ) + β·ϑ
+                #   Also, ∇_ω H = γ·ω
+                # We thus obtain the following derivatives:
+                nabla_theta = 0.5 * self.alpha * loss_gradient + self.beta * params
+                nabla_omega = self.gamma * momenta
+
+                # The gradient is just the concatenation ∇H = (∇_ϑ H, ∇_ω H)
+                gradient = tf.concat([nabla_theta, nabla_omega], 0)
+
+                out = F * gradient.numpy()
+
+                return out
+
+        else:
+            # This is the function which describes the parameter dynamics and
+            # will be used for integration
+            def dynamics(t, state):
+                state = tf.convert_to_tensor(state)
+
+                params = state[:p]
+                momenta = state[p:]
+
+                # Determine the gradient of the hamiltonian
+                with tf.GradientTape(persistent=True) as tape:
+                    tape.watch(momenta)
+
+                    system_energy = hamiltonian(params, momenta)
+
+                grad_theta = tape.gradient(system_energy, model.trainable_variables)
+                grad_omega = tape.gradient(system_energy, momenta)
+
+                del tape
+
+                grad_theta = _flatten_variables(grad_theta)
+                gradient = tf.concat([grad_theta, grad_omega], 0)
+
+                out = F * gradient.numpy()
+
+                return out
+
+        return dynamics, hamiltonian
+
+    def get_hamiltonian(
+            self, model: keras.models.Model, inputs: tf.Tensor, targets: tf.Tensor
+        ):
+        """Generate a Tensorflow instrumented function that represents the systems Hamiltonian.
+
+        """
+        p = _model_param_count(model)
+
+        @tf.function
+        def hamiltonian(params: tf.Tensor, momenta: tf.Tensor) -> float:
+            """Calculate the value of the Hamiltonian of the system.
+
+            The parameter state should be a tf.Tensor (x in the thesis, Xi in the original paper).
+
+            It takes two arguments, params and momenta, instead of the single state argument,
+            since it is possible to faster calculate the partial derivative with regard to the momenta.
+            """
+
+            # Assign state parameters to model
+            _model_set_flat_variables(model, params)
+
+            # Predict the outcome with the new parameters
+            prediction = model(inputs)
+
+            return 0.5 * (
+                  self.alpha * model.loss(targets, prediction)
+                + self.beta  * tf.tensordot(params, params, 1)
+                + self.gamma * tf.tensordot(momenta, momenta, 1)
+            )
+
+        return hamiltonian
+
+    @staticmethod
+    def _calculate_dynamics_matrix(B: np.ndarray) -> np.ndarray:
+        """This function calculates the matrix called F = J - R in the
+        paper, where J = [[0, 1],[-1, 0]] and R = [[0, 0],[0, B]]
+        (0 and 1 are the zero and identity matrix respectively).
+        It is calculated and stored as a sparse tensor in order to save
+        memory.
+
+        The parameter B must be a symmetric and positive definite p x p
+        matrix. Preferably, B is also a sparse matrix.
+
+        The types are annotated with np.ndarray, although they are
+        sparse scipy matrices. However, the matrix and its operations
+        are compatible to numpy's operations, thus the annotation.
+        """
+
+        # Get one side of B and check that B is actually square
+        # We don't check that B is symmetric and positive definite.
+        p = B.shape[0]
+        assert B.shape == (p, p)
+
+        # Create a p x p identity matrix
+        I = sparse.eye(p)
+        # Build F from its four components
+        F = sparse.bmat([[None, I], [-I, -B]])
+        # Remove the identity matrix from memory, since we don't need it
+        # anymore.
+        del I
+
+        return F
+
+
+####################################
+# MODEL VARIABLE RELATED FUNCTIONS #
+####################################
+def _flatten_variables(variables: List[tf.Variable]) -> tf.Tensor:
+    """Given a list of TensorFlow variable tensors, create a one-dimensional tensor from the variables."""
+    # This is done by concatenating the variables after reshaping them into a single dimension
+    return tf.concat([tf.reshape(var, [-1]) for var in variables], 0)
+
+
+def _model_param_count(model: keras.models.Model) -> int:
+    """Count the number of (trainable) parameters of the given model."""
+    return sum([tf.size(var) for var in model.trainable_variables])
+
+
+def _model_set_flat_variables(model: keras.models.Model, values: tf.Tensor) -> None:
+    """Set the model's variables to the values provided."""
+    variables = model.trainable_variables
+
+    offset = 0
+    for variable in variables:
+        count = tf.size(variable)
+        variable.assign(tf.reshape(values[offset : offset + count], variable.shape))
+        offset += count
---
+++ README.md
@@ -0,0 +1,60 @@
+# Port-Hamiltonian Neural Networks
+This is a TensorFlow implementation of Port-Hamiltonian Neural Networks and their training based on the methods described in the paper [Port-Hamiltonian Approach to Neural Network Training](https://arxiv.org/abs/1909.02702v1) by Stefano Massaroli, Michael Poli, et.al. (arXiv:1909.02702v1 [cs.NE]).
+
+It is written for TensorFlow 2.1.0, but it may work with future versions as well.
+A main focus in development was usability and flexibility.
+The goal was that this optimizer works with all regular models, i.e. models to which other, more popular methods, such as SGD, could be applied as well.
+
+## Ergonomics
+Due to API limitations of the TensorFlow library, it is unfortunately not possible to pass the custom optimizer to Keras' `model.compile()` function and use `model.fit()` to train the model afterwards.
+The problem here is that with the Keras optimizer interface, the optimizer only ever gets to see the gradient of the current batch for the current set of model parameters.
+However, it does not have access to the batch itself, which we need for the hamiltonian.
+It is also not possible to use the hamiltonian as loss function, since the gradient needs to be evaluated at multiple points (i.e. for multiple parameters) by the IVP solvers.
+
+## Example
+The following example shows how a model can be trained for the ubiquitous MNIST dataset using this optimizer:
+```python
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from PHNetworks.PHOptimizer import PortHamiltonianOptimizer as PHOpt
+
+def make_model():
+    keras.backend.set_floatx('float64')
+    model = keras.models.Sequential(
+        layers=[
+            # Make 28x28-entry two-dimensional input 784-entry one-dimensional
+            layers.Flatten(input_shape=(28, 28), name='input_layer'),
+            # Hidden layer: 32 nodes with ReLU activation function
+            layers.Dense(32, activation='relu', name='hidden_layer'),
+            layers.Dropout(0.2),
+            # Output layer: 10 nodes for 10 possible digits
+            layers.Dense(10, activation='softmax', name='output_layer')
+        ],
+        name='mnist_model')
+    model.summary()
+    return model
+
+model = make_model()
+optimizer = PHOpt()
+model.compile(loss=keras.losses.CategoricalCrossentropy())
+
+# Load MNIST handwritten digits dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+# Prescale pixel byte brightness to float in range [0, 1]
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Convert integer category to activation of the output neurons
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test  = keras.utils.to_categorical(y_test, 10)
+
+# Train the model
+optimizer.train(model, x_train, y_train, epochs=4)
+```
+
+## Copyright
+&copy; 2020 Daniel Albert
+
+This implementation has been written as a part of my [bachelor thesis](https://proj.esclear.de/bachelor-thesis).  
+As such I provided it under the terms of the [GNU General Public License v3.0](LICENSE).
