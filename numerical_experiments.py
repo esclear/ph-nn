b49a6d121d7cc11a4c29141db6026cc5f3915e11
Daniel Albert <esclear@users.noreply.github.com>
2020-06-10 12:56:07 +0200

    Add parameters to numerical experiments

--- numerical_experiments.py
+++ numerical_experiments.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 import itertools
 from timeit import default_timer as timer
@@ -13,7 +13,9 @@ from PHOptimizer import PortHamiltonianOptimizer as PHOpt
 
 DEPTHS = [0, 1, 2, 4, 6]
 LAYER_SIZES = [1000, 500, 250, 50]
-
+IVP_PERIOD = 5.0
+BATCH_SIZE = 64
+START_WITH_LARGEST = False
 
 #########################################
 # MODEL-CREATION-SPECIFIC FUNCTIONALITY #
@@ -64,10 +66,6 @@ def has_repetitions(iter, count=3):
 # Load MNIST handwritten digits dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-# We only want to train with a subset of training data, due to the long runtime
-# for the sequential training.
-training_sample_count = 10_000
-x_train, y_train = x_train[:training_sample_count], y_train[:training_sample_count]
 
 # Prescale pixel byte brightness to float in range [0, 1]
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -82,14 +80,14 @@ def train_and_evaluate(sizes, id=None):
     
     model = make_model(sizes)
     
-    optimizer = PHOpt(ivp_period=2.0, ivp_method='RK23')
+    optimizer = PHOpt(ivp_period=IVP_PERIOD, ivp_method='RK23')
     
     print(f'DESC: Training model {label}')
     start = timer()
     
     optimizer.train(
         model, x_train, y_train,
-        batch_size=64,
+        batch_size=BATCH_SIZE,
         epochs=2,
         metrics=[keras.metrics.CategoricalAccuracy()],
         callbacks=[
@@ -110,6 +108,7 @@ def exec():
     layer_sizes = get_layer_sizes(DEPTHS)
     layer_sizes = itertools.filterfalse(has_repetitions, layer_sizes)
     layer_sizes = list(layer_sizes)
+    layer_sizes = sorted(layer_sizes, key=sum, reverse=START_WITH_LARGEST)
     
     print(f'COUN: Training {len(layer_sizes)} networks')
     
