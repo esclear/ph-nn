6e146291c8766527d10607c35973b54a96604e81
Daniel Albert <esclear@users.noreply.github.com>
2020-07-06 02:06:17 +0200

    Update architecture evaluation table format

--- architecture_evaluation/prepare.sh
+++ architecture_evaluation/prepare.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep '>>> ' full_log.txt | cut -d ',' -f 2- |  sort -t ',' -h -k 3,3 | sort -t ',' -s -h -k 1,1 | sed 's/%/\\,\\%/g' | sed 's/\./\{,\}/g' | grep -E '^[0-4a-z]' > stripped.txt
+grep '>>> ' full_log.txt | cut -d ',' -f 2- |  sort -t ',' -h -k 3,3 | sort -t ',' -s -h -k 1,1 | sed -E 's/([0-9]+)(%|s)/\1/g' | sed 's/\./\{,\}/g' | grep -E '^[0-4a-z]' > stripped.txt
